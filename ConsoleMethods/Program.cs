3bb2acd4282143f1beecf8f70170ccec489cfe27
JavierNegrete <janedaz@hotmail.com>
2017-08-30 23:16:35 -0500

    Add files via upload

---
+++ ConsoleMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            Console.WriteLine("**********Introduce one number between 1 and 10**********");
+            n = Console.ReadLine();
+            Console.WriteLine("*************************************");
+            Multiplica(int.Parse(n));
+            Console.ReadLine();
+        }
+
+        static void Multiplica(int num)
+        {
+            int res = 1;
+            for (int i = 1; i < 11; i++)
+            {
+                res = num * i;
+                Console.WriteLine(num + "   *   " + i + "   =   "+ res);
+            }
+        }
+    }
+}
